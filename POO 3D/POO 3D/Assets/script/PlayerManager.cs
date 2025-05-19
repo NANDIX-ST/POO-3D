4df275606050ee510198295b4c698b3ec26b069e
NANDIX-ST <fernando.santos1@escolar.ifrn.edu.br>
2025-05-19 17:52:28 -0300

    andando

--- POO 3D/POO 3D/Assets/script/PlayerManager.cs
+++ POO 3D/POO 3D/Assets/script/PlayerManager.cs
@@ -2,13 +2,17 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    private MovimentoPlayer movimentoPlayer;
+    public Animator animator;
     
      void Start()
     {
-        
+        movimentoPlayer = gameObject.GetComponent<MovimentoPlayer>();
+        animator.speed = 5;
     }
    void Update()
     {
-        
+            animator.SetBool("Andando", movimentoPlayer.EstraAndando());
+           
     }
 }
